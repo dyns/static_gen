038d79c0d4a73d619c75bcdbf8c88bfe42dd9859
dyns <dyns@users.noreply.github.com>
2019-03-03 18:32:01 -0700

    format pages into directories with index pages instead of plain html pages, so the url doesn't have to have .html extension

--- py_gen/build_main.py
+++ py_gen/build_main.py
@@ -21,7 +21,7 @@ def generate_archive_html(processed_posts, posts_slug, post_previews):
     buf = []
 
     for post, preview in zip(processed_posts, post_previews):
-        url = './' + posts_slug + '/'+ post[2]
+        url = '/' + posts_slug + '/'+ post[2]
         p_html = '<div class="archiveEntry"><h3><a href="{}">{}</a></h3> {} <hr /> </div>'.format( url, post[0], preview) #str(post[1]) 
         buf.append(p_html)
 
@@ -58,7 +58,7 @@ def generate_topic_to_posts_table(processed_posts):
 
 def generate_topic_pages_links(topics_slug, post_topics):
     topic_slugs = [s.lower().replace(' ', '_') for s in post_topics]
-    topic_links = ['{topics_slug}/{slug}.html'.format(topics_slug=topics_slug, slug=slug) for (slug, name) in zip(topic_slugs, post_topics)]
+    topic_links = ['{topics_slug}/{slug}'.format(topics_slug=topics_slug, slug=slug) for (slug, name) in zip(topic_slugs, post_topics)]
     topic_anchor_tags = ['<a href="/{topic_link}">{name}</a>'.format(topic_link=topic_link, name=name) for (topic_link, name) in zip(topic_links, post_topics)]
     return (topic_links, topic_anchor_tags)
 
@@ -66,11 +66,11 @@ def format_topics_html(topic_anchor_tags):
     return ['{l}<br />'.format(l=l) for l in topic_anchor_tags]
 
 # returns [('template_name_html', 'the inflated html')]
-def inflate_partials(partials_folder, post_topics, topic_links, config_tabs, site_name):
+def inflate_partials(partials_folder, post_topics, topic_links, config_tabs, site_name, google_analytics_id):
     templates_html = {}
 
     partial_injects = {}
-    partial_injects.update({'topic_data': zip(post_topics, topic_links), 'topic_links': topic_links, 'topic_names': post_topics})
+    partial_injects.update({'topic_data': zip(post_topics, topic_links), 'topic_links': topic_links, 'topic_names': post_topics, 'google_analytics_id': google_analytics_id})
     partial_injects.update({'tabs': config_tabs, 'site_name':site_name})
 
     for file_name in os.listdir(partials_folder):
@@ -79,6 +79,13 @@ def inflate_partials(partials_folder, post_topics, topic_links, config_tabs, sit
         templates_html.update({file_name: partial_renderer.gen(html_text_r, partial_injects)})
 
     return templates_html
+
+# returns path to where new file should be
+def create_page_as_folder(output_folder, page_title):
+    new_folder_path = os.path.join(output_folder, page_title)
+    new_file_path = os.path.join(new_folder_path, 'index.html')
+    os.mkdir(new_folder_path, mode=0o700)
+    return new_file_path
 
 def do_build(config):
     
@@ -104,9 +111,8 @@ def do_build(config):
     topics_slug = str(config['topics_slug'])
     config_tabs = config['tabs']
 
+    google_analytics_id = config['google_analytics_id']
    
-
-
     posts_output_folder = output_folder + '/' + posts_slug
     topics_output_folder = output_folder + '/' + topics_slug
 
@@ -132,8 +138,9 @@ def do_build(config):
 
     # generate topic pages links
     (topic_links, topic_anchor_tags) = generate_topic_pages_links(topics_slug, post_topics)
-
-    partials = inflate_partials(partials_folder, post_topics, topic_links, config_tabs, site_name)
+    print('anchor tags', topic_links)
+    
+    partials = inflate_partials(partials_folder, post_topics, topic_links, config_tabs, site_name, google_analytics_id)
 
     # inflate posts
     (post_topics, processed_posts, post_previews) = process_posts(DATE_FORMAT, posts_folder, posts_output_folder, post_template_path, partials)
@@ -143,18 +150,34 @@ def do_build(config):
             if file_name.endswith('.md'):
                 full_path = os.path.join(pages_dir, file_name)
                 with open(full_path, 'r') as f:
-                    new_name = file_name[:-3].replace(' ', '_') + '.html'
+                    new_title = file_name[:-3].replace(' ', '_')
+
                     # convert md to html
                     html_content = markdown.markdown(f.read())
                     injects.update({'content':html_content, 'title': site_name, 'site_name':site_name})
-                    out_path = os.path.join(output_folder, new_name)
+                    
+                    if 'index' in full_path:
+                        # dont create a folder
+                        new_file_name = new_title + '.html'
+                        out_path = os.path.join(output_folder, new_file_name)
+                    else:
+                        out_path = create_page_as_folder(output_folder, new_title)
+                        # turn page into index page of folder with name
+                        #new_folder_path = os.path.join(output_folder, new_title)
+                        #out_path = os.path.join(output_folder, new_title, 'index.html')
+                        # create folder with name
+                        #os.mkdir(new_folder_path, mode=0o700)
 
+                    jinja_template_inject(j_pages_template_path, out_path, injects)
+                    
+                    '''
                     if 'index' in full_path:
                         jinja_template_inject(j_pages_template_path, out_path, injects)
                     else:
                         template_inject(injects, 
                             pages_template_path,
                             out_path)
+                    '''
 
     # inflate pages
     process_pages(pages_folder, partials)
@@ -176,7 +199,8 @@ def do_build(config):
     inject_data = {'site_name':site_name, 'archive_content': archive_html, 'archive_out_title':archive_out_title}
     inject_data.update(partials)
 
-    jinja_template_inject(archive_template_path, os.path.join(output_folder, archive_out_slug), inject_data)
+    archive_path = create_page_as_folder(output_folder, archive_out_slug)
+    jinja_template_inject(archive_template_path, archive_path, inject_data)
 
     # create topics folder
     os.mkdir(topics_output_folder, mode=0o700)
@@ -186,4 +210,10 @@ def do_build(config):
         topic_content = ['<a href="/{posts_slug}/{post_slug}">{post_name}</a>'.format(posts_slug=posts_slug, post_slug=post_slug,post_name=post_name) for (post_name, post_slug) in topics_to_posts[topic]]
         topic_content = '<br />'.join(topic_content)
         inject_data.update({'content': topic_content, 'title': topic})
-        template_inject(inject_data, topic_template_path, os.path.join(output_folder, topic_link))
+       
+        print(topic_link)
+
+        topic_page_path = create_page_as_folder(output_folder, topic_link)
+        template_inject(inject_data, topic_template_path, topic_page_path)
+
+        #template_inject(inject_data, topic_template_path, os.path.join(output_folder, topic_link))
